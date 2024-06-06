78093e73610ac257bce966e2d2f180b695b8107d
Giannis Clipper <giannisclipper@gmail.com>
2024-06-06 07:35:51 +0300

    Add database file.

--- helpers/test.js
+++
@@ -1,7 +0,0 @@
-const { rangeToDates, rangeToCounter } = require( './date.js' ); 
-
-console.log( rangeToDates( '2024-04-30', '2023-05-01' ) );
-console.log( rangeToDates( '2024-04-30', '2024-05-01' ) );
-console.log( rangeToDates( '2024-04-30', '2024-05-03' ) );
-console.log( rangeToCounter( '2024-04-30', '2024-05-03' ) );
-console.log( rangeToCounter( '2024-05-30', '2024-05-03' ) );
